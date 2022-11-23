fab197e5b2a904bd9191418f7d6233c23c682e17
lorobert <lorobert@student.42lausanne.ch>
2022-11-23 10:27:58 +0100

    final version

--- ft_checker.c
+++ ft_checker.c
@@ -6,7 +6,7 @@
 /*   By: lorobert <lorobert@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 14:01:24 by lorobert          #+#    #+#             */
-/*   Updated: 2022/11/22 14:18:44 by lorobert         ###   ########.fr       */
+/*   Updated: 2022/11/23 10:22:18 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,37 +44,48 @@ int	ft_execute(char *command, t_stack *a, t_stack *b)
 	return (0);
 }
 
+static void	init(t_tab **sorted, t_stack **a, t_stack **b)
+{
+	*sorted = ft_init_tab();
+	*a = ft_init_stack('a');
+	if (!*a)
+		ft_error(NULL, NULL, *sorted, "Error\n");
+	*b = ft_init_stack('b');
+	if (!*b)
+		ft_error(*a, NULL, *sorted, "Error\n");
+}
+
+static void	read_commands(t_stack *a, t_stack *b, t_tab *sorted)
+{
+	char	*command;
+
+	command = get_next_line(1);
+	while (command)
+	{
+		if (ft_execute(command, a, b))
+			ft_error(a, b, sorted, "Error\n");
+		command = get_next_line(1);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
 	t_stack	*b;
 	t_tab	*sorted;
-	char	*command;
 
 	if (argc == 1)
 		return (0);
-	sorted = ft_init_tab();
-	a = ft_init_stack('a');
-	if (!a)
-		ft_error(NULL, NULL, sorted, "KO\n");
-	b = ft_init_stack('b');
-	if (!b)
-		ft_error(a, NULL, sorted, "KO\n");
+	init(&sorted, &a, &b);
 	if (argc == 2 && ft_strchr(argv[1], ' '))
 		a->size = parse_string(argv[1], &a->list, sorted);
 	else
 		a->size = parse_args(argc, argv, &a->list, sorted);
 	if (!a->size)
-		ft_error(a, b, sorted, "KO\n");
+		ft_error(a, b, sorted, "Error\n");
 	if (ft_check_duplicates(sorted))
-		ft_error(a, b, sorted, "KO\n");
-	command = get_next_line(1);
-	while (command)
-	{
-		if (ft_execute(command, a, b))
-			ft_error(a, b, sorted, "KO\n");
-		command = get_next_line(1);
-	}
+		ft_error(a, b, sorted, "Error\n");
+	read_commands(a, b, sorted);
 	if (ft_is_sorted(a))
 		ft_printf("OK\n");
 	else
